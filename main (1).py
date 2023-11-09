4c38bec9a41db121bf438d2ccb5ffa33d12fdde8
cepon99 <149647169+cepon99@users.noreply.github.com>
2023-11-09 01:56:26 -0500

    Add files via upload

--- main (1).py
+++ main (1).py
@@ -1,10 +1,9 @@
 import pygame
 import os
 import random
-ANCHO=1280
-LARGO=720
+
 pygame.init()
-screen = pygame.display.set_mode((ANCHO, LARGO))
+screen = pygame.display.set_mode((800, 600))
 
 pygame.display.set_caption("Jetpack Joyride")
 
@@ -12,19 +11,37 @@ jetpack_image = pygame.image.load(os.path.join('boy.png'))
 obstacle_image = pygame.image.load(os.path.join('jetpack_2-removebg-preview (1).png'))
 background_image = pygame.image.load(os.path.join('B21rglpIUAEAa4Z.jpg'))
 game_over_image = pygame.image.load(os.path.join('1258544.jpg'))
-PLAYER_X=50
-PLAYER_Y=480
-y_change=0
-x_change=0
-gravedad=1
-obstacle_x = 800
-obstacle_y = random.randint(100, 400)
-obstacle_width = 100
-obstacle_height = 100
+moneda_image = pygame.image.load(os.path.join('moneda.png')).convert_alpha()
 
+monedas = [] 
+obstaculos = [] 
+GENERAR_MONEDAS_EVENTO = pygame.USEREVENT + 1
+GENERAR_OBSTACULO_EVENTO = pygame.USEREVENT + 2
 
-scaled_image = pygame.transform.scale(background_image, (ANCHO, LARGO))
-scaled_image2 = pygame.transform.scale(game_over_image, (ANCHO, LARGO))
+def generar_fila_de_monedas(y):
+    fila_de_monedas = []
+    for i in range(random.randint(5, 10)):
+        x = 800 + i * moneda_image.get_width()  # Coloca las monedas en una fila
+        fila_de_monedas.append(pygame.Rect(x, y, moneda_image.get_width(), moneda_image.get_height()))
+    return fila_de_monedas
+
+def generar_obstaculo(y):
+    x = 800  
+    return pygame.Rect(x, y, obstacle_image.get_width(), obstacle_image.get_height())
+
+pygame.time.set_timer(GENERAR_MONEDAS_EVENTO, random.randint(4000, 8000))  # Generar evento cada 4 a 8 segundos para las monedas
+pygame.time.set_timer(GENERAR_OBSTACULO_EVENTO, random.randint(8000, 15000))  # Generar evento cada 10 a 15 segundos para el obstáculo
+
+y_positions = [random.randint(100, 500) for _ in range(5)]  # Lista de posiciones en Y para las filas de monedas
+monedas.extend(generar_fila_de_monedas(y_positions[0]))  # Genera la primera fila de monedas
+
+jetpack_x = 50
+jetpack_y = 300
+jetpack_width = jetpack_image.get_width()
+jetpack_height = jetpack_image.get_height()
+
+scaled_image = pygame.transform.scale(background_image, (800, 600))
+scaled_image2 = pygame.transform.scale(game_over_image, (800, 600))
 
 screen.blit(scaled_image, (0, 0))
 
@@ -32,7 +49,7 @@ clock = pygame.time.Clock()
 score = 0
 running = True
 
-
+gravity = 6  
 
 while running:
     clock.tick(60)
@@ -40,59 +57,47 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        elif event.type == GENERAR_MONEDAS_EVENTO:
+            monedas.extend(generar_fila_de_monedas(y_positions[random.randint(0, 4)]))
+        elif event.type == GENERAR_OBSTACULO_EVENTO:
+            y = random.choice([pos for pos in y_positions if pos not in [moneda_rect.y for moneda_rect in monedas]])
+            obstaculos.append(generar_obstaculo(y))
 
-    if event.type==pygame.KEYDOWN:
-        if event.key==pygame.K_UP and y_change==0:
-            y_change += 10
-        elif event.key==pygame.K_DOWN:
-            y_change -= 2
-        elif event.key==pygame.K_LEFT:
-            y_change -=1
-        if event.key==pygame.K_RIGHT:
-            y_change +=1
-    if event.type==pygame.KEYUP:
-        if event.key==pygame.K_RIGHT:
-            y_change=0
-        if event.key==pygame.K_LEFT:
-            y_change=0
-    
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_LEFT] and jetpack_x > 0:
+        jetpack_x -= 5
+    if keys[pygame.K_RIGHT] and jetpack_x < 700 - jetpack_width:
+        jetpack_x += 5
+    if keys[pygame.K_UP] and jetpack_y > 0:
+        jetpack_y -= 13
+
+    if jetpack_y < 600 - jetpack_height:
+        jetpack_y += gravity
 
     screen.blit(scaled_image, (0, 0))
 
-    obstacle_x -= 5
-    if obstacle_x + obstacle_width < 0:
-        obstacle_x = 800
-        obstacle_y = random.randint(100, 500)
+    for moneda_rect in monedas:
+        moneda_rect.x -= 5
+        screen.blit(moneda_image, moneda_rect)
 
-    if 0<=PLAYER_X<=430:
-        PLAYER_X+=x_change
-    if PLAYER_X<0:
-        PLAYER_X=0
-    if PLAYER_X>430:
-        PLAYER_X=430
+    for obstaculo_rect in obstaculos:
+        obstaculo_rect.x -= 5
+        screen.blit(obstacle_image, obstaculo_rect)
 
-    if y_change > 0 or PLAYER_Y < 480:
-        PLAYER_Y-=y_change
-        y_change-=gravedad
-    if PLAYER_Y>480:
-        PLAYER_Y=480
-    if PLAYER_Y==480  and y_change<0:
-        y_change=0
+    screen.blit(jetpack_image, (jetpack_x, jetpack_y))
 
-    screen.blit(obstacle_image, (obstacle_x, obstacle_y))
-    screen.blit(jetpack_image, (PLAYER_X, PLAYER_Y))
+    for moneda_rect in monedas:
+        if jetpack_x < moneda_rect.x < jetpack_x + jetpack_width and jetpack_y < moneda_rect.y < jetpack_y + jetpack_height:
+            monedas.remove(moneda_rect)
+            score += 1
 
-    if (y_change + jetpack_image.get_width() > obstacle_x and
-        y_change < obstacle_x + obstacle_width and
-        x_change + jetpack_image.get_height() > obstacle_y and
-        x_change < obstacle_y + obstacle_height):
-        screen.blit(scaled_image2, (0, 0))
-        pygame.display.flip()
-
-        pygame.time.delay(3000)
-        running = False
+    for obstaculo_rect in obstaculos:
+        if jetpack_x < obstaculo_rect.x < jetpack_x + jetpack_width and jetpack_y < obstaculo_rect.y < jetpack_y + jetpack_height:
+            screen.blit(scaled_image2, (0, 0))
+            pygame.display.flip()
+            pygame.time.delay(3000)
+            running = False
 
     pygame.display.flip()
-
 
 pygame.quit()
