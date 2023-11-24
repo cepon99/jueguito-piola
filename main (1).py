16b482fc190a40d1434a43b766fd1e5ce35aacbf
cepon99 <149647169+cepon99@users.noreply.github.com>
2023-11-24 01:41:11 -0500

    Add files via upload

--- main (1).py
+++ main (1).py
@@ -3,21 +3,37 @@ import os
 import random
 
 pygame.init()
-screen = pygame.display.set_mode((800, 600))
+ancho = 800
+alto = 600
+screen = pygame.display.set_mode((ancho, alto))
+pygame.display.set_caption("Cat, run!")
 
-pygame.display.set_caption("Jetpack Joyride")
+# Música de fondo
+pygame.mixer.music.load("musica.mpeg")
+pygame.mixer.music.play(-1)
 
-moneda_image1 = pygame.image.load(os.path.join('moneda.png')).convert_alpha()
-jetpack_image = pygame.image.load(os.path.join('boy.png')).convert_alpha()
-obstacle_image = pygame.image.load(os.path.join('jetpack_2-removebg-preview (1).png')).convert_alpha()
-background_image = pygame.image.load(os.path.join('B21rglpIUAEAa4Z.jpg')).convert_alpha()
-game_over_image = pygame.image.load(os.path.join('1258544.jpg')).convert_alpha()
-moneda_image = pygame.image.load(os.path.join('moneda.png')).convert_alpha()
+# Logo del juego
+logo = pygame.image.load("logo.png")
+pygame.display.set_icon(logo)
 
-monedas = [] 
-obstaculos = [] 
+jetpack_image = pygame.image.load(os.path.join("salta.png")).convert_alpha()
+obstacle_image = pygame.image.load(os.path.join("obs1.png")).convert_alpha()
+misil_image = pygame.image.load(os.path.join("misil.png")).convert_alpha()
+background_image = pygame.image.load(os.path.join("fondo1.png")).convert_alpha()
+game_over_image = pygame.image.load(os.path.join("1258544.jpg"))
+moneda_image = pygame.image.load(os.path.join("moneda.png")).convert_alpha()
+sonido_abajo = pygame.image.load(os.path.join("bajar.png")).convert_alpha()
+sonido_mute = pygame.image.load(os.path.join("apagado.png")).convert_alpha()
+sonido_subir = pygame.image.load(os.path.join("subir.png")).convert_alpha()
+imagen_linea = pygame.image.load("waos.png").convert_alpha()
+monedas = []
+obstaculos = []
+misiles = []  
+obstaculos_linea = []
 GENERAR_MONEDAS_EVENTO = pygame.USEREVENT + 1
 GENERAR_OBSTACULO_EVENTO = pygame.USEREVENT + 2
+GENERAR_MISIL_EVENTO = pygame.USEREVENT + 3  
+GENERAR_LINEA_EVENTO = pygame.USEREVENT + 4
 
 def dibujar_texto (surface, text, size,x,y):
     font= pygame.font.SysFont("serif",size)
@@ -33,15 +49,28 @@ def generar_fila_de_monedas(y):
         fila_de_monedas.append(pygame.Rect(x, y, moneda_image.get_width(), moneda_image.get_height()))
     return fila_de_monedas
 
-def generar_obstaculo(y):
-    x = 800  
+def generar_obstaculo():
+    y = random.choice(y_positions)
+    x = 800
     return pygame.Rect(x, y, obstacle_image.get_width(), obstacle_image.get_height())
 
-pygame.time.set_timer(GENERAR_MONEDAS_EVENTO, random.randint(4000, 8000))  # Generar evento cada 4 a 8 segundos para las monedas
-pygame.time.set_timer(GENERAR_OBSTACULO_EVENTO, random.randint(8000, 15000))  # Generar evento cada 10 a 15 segundos para el obstáculo
+def generar_misil(y):
+    x = 800
+    return pygame.Rect(x, y, misil_image.get_width(), misil_image.get_height())
 
-y_positions = [random.randint(100, 500) for _ in range(5)]  # Lista de posiciones en Y para las filas de monedas
-monedas.extend(generar_fila_de_monedas(y_positions[0]))  # Genera la primera fila de monedas
+def generar_obstaculo_linea():
+    y = random.randint(100, 500)
+    x1 = 800
+    x2 = x1 + random.randint(50, 200)
+    return pygame.Rect(x1, y, x2 - x1, 10)
+
+pygame.time.set_timer(GENERAR_MONEDAS_EVENTO, random.randint(3200, 6000))
+pygame.time.set_timer(GENERAR_OBSTACULO_EVENTO, random.randint(5000, 10000))
+pygame.time.set_timer(GENERAR_MISIL_EVENTO, random.randint(10000, 20000))
+pygame.time.set_timer(GENERAR_LINEA_EVENTO, random.randint(3000, 7000))
+
+
+y_positions = [random.randint(100, 500) for _ in range(10)]
 
 jetpack_x = 50
 jetpack_y = 300
@@ -51,13 +80,63 @@ jetpack_height = jetpack_image.get_height()
 scaled_image = pygame.transform.scale(background_image, (800, 600))
 scaled_image2 = pygame.transform.scale(game_over_image, (800, 600))
 
-screen.blit(scaled_image, (0, 0))
-
 clock = pygame.time.Clock()
 score = 0
 running = True
+fondo_x = 0
+fondo_velocidad = 8
+gravity = 6
 
-gravity = 6  
+
+def pantalla():
+    global fondo_x, running
+    fondo_avanza = fondo_x % background_image.get_rect().width
+    screen.blit(background_image, (fondo_avanza - background_image.get_rect().width, 0))
+    if fondo_avanza < ancho:
+        screen.blit(background_image, (fondo_avanza, 0))
+    fondo_x -= fondo_velocidad
+
+    for moneda_rect in monedas:
+        moneda_rect.x -= fondo_velocidad
+        screen.blit(moneda_image, moneda_rect)
+
+    for obstaculo_rect in obstaculos:
+        obstaculo_rect.x -= fondo_velocidad
+        screen.blit(obstacle_image, obstaculo_rect)
+
+        if (
+            jetpack_x < obstaculo_rect.x + obstaculo_rect.width - 10
+            and jetpack_x + jetpack_width - 10 > obstaculo_rect.x
+            and jetpack_y < obstaculo_rect.y + obstaculo_rect.height - 10
+            and jetpack_y + jetpack_height - 10 > obstaculo_rect.y
+        ):
+            screen.blit(scaled_image2, (0, 0))
+            pygame.display.flip()
+            pygame.time.delay(3000)
+            running = False
+
+    for obstaculo_rect, _ in obstaculos_linea:
+        obstaculo_rect.x -= fondo_velocidad
+        x, y, width, height = obstaculo_rect
+        # Dibuja la línea roja
+        pygame.draw.rect(screen, (255, 0, 0), pygame.Rect(x, y, width, height))
+
+        if (
+            jetpack_x < x + width - 10
+            and jetpack_x + jetpack_width - 10 > x
+            and jetpack_y < y + height - 10
+            and jetpack_y + jetpack_height - 10 > y
+        ):
+            screen.blit(scaled_image2, (0, 0))
+            pygame.display.flip()
+            pygame.time.delay(3000)
+            running = False
+
+    for misil_rect in misiles:
+        misil_rect.x -= fondo_velocidad + 5
+        screen.blit(misil_image, misil_rect)
+
+    screen.blit(jetpack_image, (jetpack_x, jetpack_y))
 
 while running:
     clock.tick(60)
@@ -66,10 +145,17 @@ while running:
         if event.type == pygame.QUIT:
             running = False
         elif event.type == GENERAR_MONEDAS_EVENTO:
-            monedas.extend(generar_fila_de_monedas(y_positions[random.randint(0, 4)]))
+            monedas.extend(generar_fila_de_monedas(y_positions[random.randint(0, 9)]))
         elif event.type == GENERAR_OBSTACULO_EVENTO:
             y = random.choice([pos for pos in y_positions if pos not in [moneda_rect.y for moneda_rect in monedas]])
-            obstaculos.append(generar_obstaculo(y))
+            y = max(100, min(y, 500))  # Asegura que la posición y esté dentro de los límites
+            obstaculos.append(generar_obstaculo())
+        elif event.type == GENERAR_MISIL_EVENTO:
+            y = random.choice([pos for pos in y_positions if pos not in [moneda_rect.y for moneda_rect in monedas]])
+            misiles.append(generar_misil(y))
+        elif event.type == GENERAR_LINEA_EVENTO:
+            obstaculo_rect = generar_obstaculo_linea()
+            obstaculos_linea.append((obstaculo_rect, imagen_linea))
 
     keys = pygame.key.get_pressed()
     if keys[pygame.K_LEFT] and jetpack_x > 0:
@@ -78,6 +164,22 @@ while running:
         jetpack_x += 5
     if keys[pygame.K_UP] and jetpack_y > 0:
         jetpack_y -= 13
+    
+    # Volumen baja
+    if keys[pygame.K_o] and pygame.mixer.music.get_volume() > 0.0:
+        pygame.mixer.music.set_volume(pygame.mixer.music.get_volume() - 0.01)
+        screen.blit(sonido_abajo, (100, 87))
+    elif keys[pygame.K_o] and pygame.mixer.music.get_volume() == 0.0:
+        screen.blit(sonido_mute, (100, 87))
+    
+    # Volumen sube
+    if keys[pygame.K_p] and pygame.mixer.music.get_volume() < 1.0:
+        pygame.mixer.music.set_volume(pygame.mixer.music.get_volume() + 0.01)
+        screen.blit(sonido_subir, (100, 87))
+    # Desactivar volumen 
+    elif keys[pygame.K_i]:
+        pygame.mixer.music.set_volume(0.0)
+        screen.blit(sonido_mute, (100, 87))
 
     if jetpack_y < 600 - jetpack_height:
         jetpack_y += gravity
@@ -85,30 +187,47 @@ while running:
     screen.blit(scaled_image, (0, 0))
 
     for moneda_rect in monedas:
-        moneda_rect.x -= 5
+        moneda_rect.x -= fondo_velocidad
         screen.blit(moneda_image, moneda_rect)
 
-    for obstaculo_rect in obstaculos:
-        obstaculo_rect.x -= 5
-        screen.blit(obstacle_image, obstaculo_rect)
+    for obstaculo_rect, imagen_linea in obstaculos_linea:
+        obstaculo_rect.x -= fondo_velocidad
+        screen.blit(imagen_linea, obstaculo_rect.topleft)
 
-    screen.blit(jetpack_image, (jetpack_x, jetpack_y))
+        if (
+            jetpack_x < obstaculo_rect.x + obstaculo_rect.width - 10
+            and jetpack_x + jetpack_width - 10 > obstaculo_rect.x
+            and jetpack_y < obstaculo_rect.y + obstaculo_rect.height - 10
+            and jetpack_y + jetpack_height - 10 > obstaculo_rect.y
+        ):
+            screen.blit(scaled_image2, (0, 0))
+            pygame.display.flip()
+            pygame.time.delay(3000)
+            running = False
 
+    for misil_rect in misiles:
+        misil_rect.x -= fondo_velocidad + 5  # Ajusta la velocidad de los misiles
+        screen.blit(misil_image, misil_rect)
+
+    # Verificar colisiones con las monedas
     for moneda_rect in monedas:
         if jetpack_x < moneda_rect.x < jetpack_x + jetpack_width and jetpack_y < moneda_rect.y < jetpack_y + jetpack_height:
             monedas.remove(moneda_rect)
             score += 1
 
-    for obstaculo_rect in obstaculos:
-        if jetpack_x < obstaculo_rect.x < jetpack_x + jetpack_width and jetpack_y < obstaculo_rect.y < jetpack_y + jetpack_height:
+    # Verificar colisiones con los misiles
+    for misil_rect in misiles:
+        if jetpack_x < misil_rect.x < jetpack_x + jetpack_width and jetpack_y < misil_rect.y < jetpack_y + jetpack_height:
             screen.blit(scaled_image2, (0, 0))
             pygame.display.flip()
             pygame.time.delay(3000)
             running = False
-    
+
+    # Eliminar misiles que salieron de la pantalla
+    misiles = [misil for misil in misiles if misil.x > -misil.width]
+    pantalla()
     dibujar_texto(screen, str(score),25 ,70,10)
-    screen.blit(moneda_image1,(25,9))
-    
+    screen.blit(moneda_image,(25,9))
     pygame.display.flip()
 
 pygame.quit()
